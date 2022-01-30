cabcdf377b7da013d6fd8f2c21ef84e3a404a7e3
mobak88 <mostafa0401@ymail.com>
2022-01-30 18:01:44 +0100

    Making data change according to daily, weekly and monthly timeframe

--- js/main.js
+++ js/main.js
@@ -4,14 +4,81 @@ const details = JSON.parse(reportDetails);
 const time = document.querySelectorAll('.card__small--time');
 const previous = document.querySelectorAll('.small__card--previous');
 
+const activeTimeframeDaily = document.getElementById('daily');
+const activeTimeframeWeekly = document.getElementById('weekly');
+const activeTimeframeMonthly = document.getElementById('monthly');
+
+/* const loopThroughReportDetails = function(pastOrPresence, timeframe, current, lastWeek) {
+    for (let i = 0;i <= details.length - 1;i++) {
+        pastOrPresence[i].innerHTML = `${details[i].timeframes.timeframe.current}hrs`;
+    } 
+} */
 
 for (let i = 0;i <= details.length - 1;i++) {
-    console.log(details[i].timeframes.daily.current);
-
     time[i].innerHTML = `${details[i].timeframes.daily.current}hrs`;
 }
 
-for (let i = 0;i <= details.length;i++) {
+for (let i = 0;i <= details.length - 1;i++) {
     previous[i].innerHTML += `Last week - ${details[i].timeframes.daily.previous}hrs`;
 }
 
+const clearTimeFrame = function() {
+    for (let i = 0;i <= details.length - 1;i++) {
+        time[i].innerHTML = '';
+    }
+
+    for (let i = 0;i <= details.length - 1;i++) {
+        previous[i].innerHTML += '';
+    }
+
+};
+
+const changeActiveTimeframe = function(timeframe, removeFirst, removeSecond) {
+    timeframe.classList.add('user-card__timeframe--active');
+    removeFirst.classList.remove('user-card__timeframe--active');
+    removeSecond.classList.remove('user-card__timeframe--active');
+
+    clearTimeFrame();
+};
+
+// activeTimeframeDaily.onclick = changeActiveTimeframe(activeTimeframeWeekly, '2');
+// activeTimeframeWeekly.onclick = changeActiveTimeframe(activeTimeframeWeekly, '2');
+// activeTimeframeMonthly.onclick = changeActiveTimeframe(activeTimeframeMonthly, '3');
+
+activeTimeframeDaily.addEventListener('click', function() {
+    changeActiveTimeframe(activeTimeframeDaily, activeTimeframeWeekly, activeTimeframeMonthly);
+
+    for (let i = 0;i <= details.length - 1;i++) {
+        time[i].innerHTML = `${details[i].timeframes.daily.current}hrs`;
+    }
+
+    for (let i = 0;i <= details.length - 1;i++) {
+        previous[i].innerHTML += `Last week - ${details[i].timeframes.daily.previous}hrs`;
+    }
+});
+
+activeTimeframeWeekly.addEventListener('click', function() {
+    changeActiveTimeframe(activeTimeframeWeekly, activeTimeframeDaily, activeTimeframeMonthly);
+
+    for (let i = 0;i <= details.length - 1;i++) {
+        time[i].innerHTML += `${details[i].timeframes.weekly.current}hrs`;
+    }
+
+
+    for (let i = 0;i <= details.length - 1;i++) {
+        previous[i].innerHTML += `Last week - ${details[i].timeframes.weekly.previous}hrs`;
+    }
+});
+
+activeTimeframeMonthly.addEventListener('click', function() {
+    changeActiveTimeframe(activeTimeframeMonthly, activeTimeframeDaily, activeTimeframeWeekly);
+
+    for (let i = 0;i <= details.length - 1;i++) {
+        time[i].innerHTML += `${details[i].timeframes.monthly.current}hrs`;
+    }
+
+
+    for (let i = 0;i <= details.length - 1;i++) {
+        previous[i].innerHTML += `${details[i].timeframes.monthly.previous}hrs`;
+    }
+});
