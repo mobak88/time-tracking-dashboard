6123a999bed8d3a79a677adcc0174028a8d93b5c
mobak88 <mostafa0401@ymail.com>
2022-01-30 13:03:02 +0100

    Adding daily data

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 import reportDetails from './reportDetails.js';
-
 const details = JSON.parse(reportDetails);
 
-for (let i = 0; i <= details.length; i++) {
-  console.log(details[i].title);
-  console.log(i);
+const time = document.querySelectorAll('.card__small--time');
+const previous = document.querySelectorAll('.small__card--previous');
+
+
+for (let i = 0;i <= details.length - 1;i++) {
+    console.log(details[i].timeframes.daily.current);
+
+    time[i].innerHTML = `${details[i].timeframes.daily.current}hrs`;
 }
+
+for (let i = 0;i <= details.length;i++) {
+    previous[i].innerHTML += `Last week - ${details[i].timeframes.daily.previous}hrs`;
+}
+
