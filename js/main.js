afe5ec317e162e1e19647c7e5ae8136954b41bbf
mobak88 <mostafa0401@ymail.com>
2022-02-03 16:41:32 +0100

    Formating code to use fetch and asunchrounus code

--- js/main.js
+++ js/main.js
@@ -1,31 +1,43 @@
 import reportDetails from './reportDetails.js';
 const details = JSON.parse(reportDetails);
 
-const currentData = document.querySelectorAll('.card__small--time');
-const previousData = document.querySelectorAll('.small__card--previous');
+const displayCurrentData = document.querySelectorAll('.card__small--time');
+const displayPreviousData = document.querySelectorAll('.small__card--previous');
 
 const activeTimeframeDaily = document.getElementById('daily');
 const activeTimeframeWeekly = document.getElementById('weekly');
 const activeTimeframeMonthly = document.getElementById('monthly');
 
-const loopThroughReportDetails = function(timeframe, data) {
+let current = true;
+
+function getData() {
+    fetch('data.json')
+        .then(res => res.json())
+        .then(data => {
+            loopThroughReportDetails(displayCurrentData, data, current);
+            loopThroughReportDetails(displayPreviousData, data);
+            console.log(data[0].timeframes.daily.current);
+        })
+        .catch(err => console.log(err));
+}
+
+function loopThroughReportDetails(timeframe, data, current) {
     if (activeTimeframeDaily.classList.contains('user-card__timeframe--active')) {
-        for (let i = 0; i <= details.length - 1; i++) {
-            timeframe[i].innerHTML = data ? `${details[i].timeframes.daily.current}hrs` : `Last week ${details[i].timeframes.daily.previous}hrs`;
+        for (let i = 0; i < data.length; i++) {
+            timeframe[i].innerHTML = current === true ? `${data[i].timeframes.daily.current}hrs` : `Last week ${data[i].timeframes.daily.previous}hrs`;
         }
     } else if (activeTimeframeWeekly.classList.contains('user-card__timeframe--active')) {
-        for (let i = 0; i <= details.length - 1; i++) {
-            timeframe[i].innerHTML = data ? `${details[i].timeframes.weekly.current}hrs` : `Last week ${details[i].timeframes.weekly.previous}hrs`;
+        for (let i = 0; i < data.length; i++) {
+            timeframe[i].innerHTML = current === true ? `${data[i].timeframes.weekly.current}hrs` : `Last week ${data[i].timeframes.weekly.previous}hrs`;
         }
     } else if (activeTimeframeMonthly.classList.contains('user-card__timeframe--active')) {
-        for (let i = 0; i <= details.length - 1; i++) {
-            timeframe[i].innerHTML = data ? `${details[i].timeframes.monthly.current}hrs` : `Last week ${details[i].timeframes.monthly.previous}hrs`;
+        for (let i = 0; i < data.length; i++) {
+            timeframe[i].innerHTML = current === true ? `${data[i].timeframes.monthly.current}hrs` : `Last week ${data[i].timeframes.monthly.previous}hrs`;
         }
     }
 };
 
-loopThroughReportDetails(currentData, details);
-loopThroughReportDetails(previousData);
+getData();
 
 const clearTimeFrame = function(timeframe) {
     for (let i = 0; i <= details.length - 1; i++) {
@@ -38,32 +50,27 @@ const changeActiveTimeframe = function(timeframe, removeFirst, removeSecond) {
     removeFirst.classList.remove('user-card__timeframe--active');
     removeSecond.classList.remove('user-card__timeframe--active');
 
-    clearTimeFrame(currentData);
-    clearTimeFrame(previousData);
+    clearTimeFrame(displayCurrentData);
+    clearTimeFrame(displayPreviousData);
 };
-
-// // How do I use onclick with argument without calling the function before the click
-// activeTimeframeDaily.onclick = changeActiveTimeframe(activeTimeframeDaily);
-// activeTimeframeWeekly.onclick = changeActiveTimeframe(activeTimeframeWeekly);
-// activeTimeframeMonthly.onclick = changeActiveTimeframe(activeTimeframeMonthly);
 
 activeTimeframeDaily.addEventListener('click', function() {
     changeActiveTimeframe(activeTimeframeDaily, activeTimeframeWeekly, activeTimeframeMonthly);
 
-    loopThroughReportDetails(currentData, details);
-    loopThroughReportDetails(previousData);
+    // loopThroughReportDetails(displayCurrentData, details);
+    // loopThroughReportDetails(displayPreviousData);
 });
 
 activeTimeframeWeekly.addEventListener('click', function() {
     changeActiveTimeframe(activeTimeframeWeekly, activeTimeframeDaily, activeTimeframeMonthly);
 
-    loopThroughReportDetails(currentData, details);
-    loopThroughReportDetails(previousData);
+    // loopThroughReportDetails(displayCurrentData, details);
+    // loopThroughReportDetails(displayPreviousData);
 });
 
 activeTimeframeMonthly.addEventListener('click', function() {
     changeActiveTimeframe(activeTimeframeMonthly, activeTimeframeDaily, activeTimeframeWeekly);
 
-    loopThroughReportDetails(currentData, details);
-    loopThroughReportDetails(previousData);
+    // loopThroughReportDetails(displayCurrentData, details);
+    // loopThroughReportDetails(displayPreviousData);
 });
